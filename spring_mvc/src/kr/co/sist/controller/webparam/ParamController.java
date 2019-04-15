cdcebeaae90cec469d70323e89b5e9a0847a298a
gkwl7878 <45455322+gkwl7878@users.noreply.github.com>
2019-04-15 18:31:51 +0900

    cm

--- spring_mvc/src/kr/co/sist/controller/webparam/ParamController.java
+++ spring_mvc/src/kr/co/sist/controller/webparam/ParamController.java
@@ -3,6 +3,7 @@ package kr.co.sist.controller.webparam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.sist.vo.ParamVO;
 
@@ -49,5 +50,24 @@ public class ParamController {
 		m.addAttribute("ageData",pv.getAge());//이 값은 JSP로 전달된다.
 		return "param/vo_process";
 	}
+	
+	@RequestMapping(value="/request_string.do", method=GET)
+	//단일형으로 매개변수를 받을 때에는 web parameter명과 method의 매개변수명이 
+	//같아야한다.
+	public String requestString(@RequestParam(defaultValue="anonymous")String name,Model m) {
+//		if(name==null||"".equals(name)) { //파라메터 자체가 없을 때
+//			//파라메터는 존재하나 값이 없을 때
+//			name="guest";
+//		}//end if
+		m.addAttribute("name",name+"님 안녕하세요?");
+		return "param/string_param";
+	}//requestString
+	
+	@RequestMapping(value="/request_int.do", method=GET)
+	public String requestInt(@RequestParam(name="age", defaultValue="1") int age, Model m) {
+		
+		m.addAttribute("age",age+"살");
+		return "param/int_param";
+	}//requestString
 	
 }
---
+++ spring_mvc/src/kr/co/sist/domain/Notice.java
@@ -0,0 +1,27 @@
+package kr.co.sist.domain;
+
+public class Notice {
+	private int num;
+	private String subject;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	@Override
+	public String toString() {
+		return "Notice [num=" + num + ", subject=" + subject + "]";
+	}
+	
+	
+	
+}
